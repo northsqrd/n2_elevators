b5984cc7c9cb70cbad94c78bb1de50cffd88040e
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-07-07 00:18:12 +0200

    feat: Add description to example floors.

--- config_s.lua
+++ config_s.lua
@@ -3,8 +3,8 @@ Config.Elevators = {
     {
         label = "Humane Labs",
         locations = {
-            {label = "Upper Floor", description = "", dest = vector4(3540.80, 3676.17, 27.12, 169.18)},
-            {label = "Lower Floor", description = "", dest = vector4(3540.80, 3676.17, 19.99, 169.18)},
+            {label = "Upper Floor", description = "Hello description here. :)", dest = vector4(3540.80, 3676.17, 27.12, 169.18)},
+            {label = "Lower Floor", description = "Hello another description here. :D", dest = vector4(3540.80, 3676.17, 19.99, 169.18)},
         }
     }
 }
