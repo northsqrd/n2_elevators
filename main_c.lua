5d1ad671985244d02ee8a21ccc4aef886bc8ac03
northsqrd <41840853+northsqrd@users.noreply.github.com>
2022-07-07 11:01:19 +0200

    tweak: add sound when opening menu, play elevator sound earlier, general code cleanup.

--- main_c.lua
+++ main_c.lua
@@ -12,10 +12,10 @@ RegisterNetEvent("n2_elevators:SendElevators", function(pElevators)
     elevators = pElevators
 end)
 
-local function DisplayHelpText(text)
+local function DisplayHelpText(text, playSound)
     SetTextComponentFormat("STRING")
     AddTextComponentString(text)
-    DisplayHelpTextFromStringLabel(0, 0, 1, -1)
+    DisplayHelpTextFromStringLabel(0, 0, (playSound == true), -1)
 end
 
 local function UseElevator(coords)
@@ -30,9 +30,9 @@ local function UseElevator(coords)
     SetGameplayCamRelativeHeading(0.0)
     Wait(time)
     NetworkFadeInEntity(playerPed, true)
+    PlaySoundFrontend(-1, "FAKE_ARRIVE", "MP_PROPERTIES_ELEVATOR_DOORS", true)
     DoScreenFadeIn(400)
     Wait(600)
-    PlaySoundFrontend(-1, "FAKE_ARRIVE", "MP_PROPERTIES_ELEVATOR_DOORS", true)
     usingElevator = false
     if elevatorMenu ~= nil then
         elevatorMenu:Visible(false)
@@ -45,15 +45,14 @@ local function OpenElevatorMenu(elevatorId, floorId)
     usingElevator = true
     elevatorMenu = NativeUI.CreateMenu("Elevator", elevators[elevatorId].label, menuPosition.x, menuPosition.y)
     menuPool:Add(elevatorMenu)
-    
-    
+
     for k, v in pairs(elevators[elevatorId].locations) do
         local label = v.label
-        
+
         if k == floorId then
             label = label .. " | Current"
         end
-        
+
         local item = NativeUI.CreateItem(label, v.description)
         if k == floorId then
             item:Enabled(false)
@@ -61,15 +60,15 @@ local function OpenElevatorMenu(elevatorId, floorId)
         end
         elevatorMenu:AddItem(item)
     end
-    
+
     menuPool:RefreshIndex()
     elevatorMenu:Visible(true)
     elevatorMenu:CurrentSelection(floorId-1)
-    
+
     elevatorMenu.OnItemSelect = function(_, _, index)
         UseElevator(elevators[elevatorId].locations[index].dest)
     end
-    
+
     CreateThread(function()
         while usingElevator do
             Wait(250)
@@ -114,9 +113,10 @@ CreateThread(function()
                 local coords = elevators[v.eleId].locations[v.floorId].dest.xyz
                 DrawMarker(27, coords.x, coords.y, coords.z + 0.05, 0.0, 0.0, 0.0, 0.0, 180.0, 0.0, 1.5, 1.5, 1.5, 255, 255, 255, 180, false, false, 2, nil, nil, false)
                 if #(pedCoords - coords) < 2.0 then
-                    DisplayHelpText("Press ~INPUT_CONTEXT~ to use the elevator.")
+                    DisplayHelpText("Press ~INPUT_CONTEXT~ to use the elevator.", true)
                     if IsControlJustPressed(0, 51) then
                         OpenElevatorMenu(v.eleId, v.floorId)
+                        PlaySoundFrontend(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", true)
                     end
                 end
             end
@@ -133,7 +133,7 @@ CreateThread(function()
         menuPool:MouseEdgeEnabled(false)
         menuPool:ControlDisablingEnabled(false)
         menuPool:ProcessMenus()
-        
+
         if usingElevator then
             if elevatorMenu ~= nil then
                 if elevatorMenu:Visible() == false then
